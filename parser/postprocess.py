d0f178757561407443ca3cbee17e4b808763fa94
Kenneth Chen <kenneth.chen@VUE-KF6PVVGG9H.local>
2023-10-10 12:07:05 -0400

    postprocessing rework

--- parser/postprocess.py
+++ parser/postprocess.py
@@ -20,7 +20,6 @@ class Encoder(json.JSONEncoder):
 def postprocess_data(data):
     data = json.loads(json.dumps(data, cls=Encoder))
     ingredients = []
-    last_tag = ""
 
     def trim_special_chars(str):
         special_chars = ["-", "'"]
@@ -29,24 +28,24 @@ def postprocess_data(data):
             str = str.replace(" " + char, char)
         str = str.replace("s'", "s' ")
         return str.strip()
-
-    def append_if_ingredient(entity):
-        nonlocal ingredients, last_tag
-        if entity["entity_group"] == "name":
-            if last_tag == "name":
-                ingredients[-1] += entity["word"]
-                ingredients[-1] = trim_special_chars(ingredients[-1])
-            else:
-                ingredients.append(trim_special_chars(entity["word"]))
-        last_tag = entity["entity_group"]
-
-    for entity in data:
-        if isinstance(entity, list):
-            for ent in entity:
-                append_if_ingredient(ent)
-            last_tag = ""
-        else:
-            append_if_ingredient(entity)
-
+    
+    def aggregate_names(group):
+        nonlocal ingredients
+        conjunctions = [",", "and", "+", "also", "plus"]
+        ingredient = ""
+        for entity in group:
+            if entity["word"] in conjunctions:
+                ingredients.append(trim_special_chars(ingredient))
+                ingredient = ""
+            if entity["entity_group"] == "name":
+                ingredient += " " + entity["word"]
+        ingredients.append(trim_special_chars(ingredient))
+        
+    if isinstance(data[0], list):
+        for group in data:
+            aggregate_names(group)
+    else:
+        aggregate_names(data)
+        
     ingredients = list(dict.fromkeys(ingredients))
     return {"ingredients": list(ingredients), "labels": data}

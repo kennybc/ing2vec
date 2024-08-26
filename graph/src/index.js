ba4772c1f7169245025d22b4491046c507f92b9d
kennybc <54950614+kennybc@users.noreply.github.com>
2024-08-26 12:16:13 -0400

    github pages graph display

---
+++ graph/src/index.js
@@ -0,0 +1,121 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { useEffect, useState } from "react";
+import Chart from "chart.js/auto";
+import { readString } from "react-papaparse";
+import { Scatter } from "react-chartjs-2";
+import zoomPlugin from "chartjs-plugin-zoom";
+
+import csv from "./data.csv";
+
+Chart.register(zoomPlugin);
+
+export default function App() {
+  const X_AXIS = "x";
+  const Y_AXIS = "y";
+  const POINT_RADIUS = 5;
+  const SHOW_GRID = true;
+
+  const [data, setData] = useState({ datasets: [] });
+
+  useEffect(() => {
+    const papaConfig = {
+      complete: (results, file) => {
+        const parsed = results.data.map(function (row) {
+          return {
+            x: row[1],
+            y: row[2],
+            name: row[0],
+          };
+        });
+        console.log(parsed);
+
+        setData({
+          datasets: [
+            {
+              label: "Ingredients",
+              backgroundColor: "black",
+              data: parsed,
+              pointRadius: POINT_RADIUS,
+              pointHoverRadius: POINT_RADIUS + 2,
+            },
+          ],
+        });
+      },
+      download: true,
+      error: (error, file) => {
+        console.log("Error while parsing:", error, file);
+      },
+    };
+    readString(csv, papaConfig);
+  }, []);
+
+  const options = {
+    scales: {
+      xAxes: {
+        scaleLabel: {
+          display: true,
+          labelString: X_AXIS,
+        },
+        gridLines: {
+          display: SHOW_GRID,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return value.toLocaleString();
+          },
+        },
+      },
+      yAxes: {
+        scaleLabel: {
+          display: true,
+          labelString: Y_AXIS,
+        },
+        gridLines: {
+          display: SHOW_GRID,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return value.toLocaleString();
+          },
+        },
+      },
+    },
+    plugins: {
+      tooltip: {
+        displayColors: false,
+        callbacks: {
+          label: function (ctx) {
+            console.log(ctx);
+            return ctx.raw.name;
+          },
+        },
+      },
+      zoom: {
+        pan: {
+          enabled: true,
+          mode: "xy",
+        },
+        zoom: {
+          wheel: {
+            enabled: true,
+          },
+          pinch: {
+            enabled: true,
+          },
+          mode: "xy",
+        },
+      },
+    },
+  };
+
+  return <Scatter options={options} data={data} />;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
